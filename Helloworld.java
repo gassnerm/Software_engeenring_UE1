88ab375ecb835292efb43a374c6a6883febd7a64
sss <gassnerm@smail.uni-koeln.de>
2020-11-06 09:17:08 +0100

    a

--- Helloworld.java
+++ Helloworld.java
@@ -3,7 +3,7 @@ public class newProgramm{
 	public static void main String (args[])
 	{
 		System.out.println("Hello World");
-		System.out.println("3 Jahre ")
+		System.out.println("3 fJahre ")
 
 	}
 
